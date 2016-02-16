433db77e727e34516efe863012e3a2f735f15d59
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-02-16 22:28:14 +0100

    Add missing files.

---
+++ tests/Message/PhpStreamTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace Jivoo\Http\Message;
+
+class PhpStreamTest extends \Jivoo\TestCase
+{
+    
+    /**
+     * @var PhpStream
+     */
+    protected $stream;
+    
+    protected function setUp()
+    {
+        $this->stream = new PhpStream(fopen('tests/data/test', 'w+'));
+    }
+    
+    protected function tearDown()
+    {
+        $this->stream->close();
+    }
+    
+    public function testReading()
+    {
+        $this->stream->write('foobar');
+        $this->stream->rewind();
+        
+        $this->assertTrue($this->stream->isReadable());
+        $this->assertTrue($this->stream->isSeekable());
+        $this->assertEquals('foobar', $this->stream);
+        $this->stream->rewind();
+        $this->assertEquals('foobar', $this->stream->getContents());
+        $this->stream->rewind();
+        $this->assertNull($this->stream->getSize());
+        $this->assertEquals('foo', $this->stream->read(3));
+        $this->assertEquals('bar', $this->stream->read(5));
+        $this->assertTrue($this->stream->eof());
+        $this->stream->rewind();
+        $this->assertEquals('foo', $this->stream->read(3));
+        $this->assertEquals('3', $this->stream->tell());
+        $this->stream->seek(2);
+        $this->assertEquals('oba', $this->stream->read(3));
+        $this->stream->seek(-2, SEEK_END);
+        $this->assertEquals('ar', $this->stream->read(3));
+        $this->stream->seek(2);
+        $this->stream->seek(2, SEEK_CUR);
+        $this->assertEquals('ar', $this->stream->read(3));
+        $this->assertTrue(is_array($this->stream->getMetadata()));
+    }
+    
+    public function testWriting()
+    {
+        $this->assertTrue($this->stream->isWritable());
+        $this->stream->write('foobar');
+        $this->stream->rewind();
+        
+        $this->stream->seek(3);
+        $this->assertEquals(4, $this->stream->write('bazz'));
+        $this->assertEquals('', $this->stream->getContents());
+        $this->assertEquals('foobazz', $this->stream);
+        $this->assertNull($this->stream->getSize());
+    }
+}
---
+++ tests/Message/RequestTraitTest.php
@@ -0,0 +1,57 @@
+<?php
+namespace Jivoo\Http\Message;
+
+class RequestTraitTest extends \Jivoo\TestCase
+{
+    
+    protected function getInstance()
+    {
+        return Request::create('?test');
+    }
+    
+    public function testMethod()
+    {
+        $request1 = $this->getInstance();
+        
+        $this->assertEquals('GET', $request1->getMethod());
+        $request2 = $request1->withMethod('POST');
+        $this->assertEquals('POST', $request2->getMethod());
+        $this->assertEquals('GET', $request1->getMethod());
+    }
+    
+    public function testRequestTarget()
+    {
+        $request1 = $this->getInstance();
+        
+        $this->assertEquals('/?test', $request1->getRequestTarget());
+        
+        $request2 = $request1->withUri(new Uri('/foo'));
+        
+        $this->assertEquals('/foo', $request2->getRequestTarget());
+        
+        $request3 = $request2->withRequestTarget('*');
+        
+        $this->assertEquals('*', $request3->getRequestTarget());
+    }
+    
+    public function testUri()
+    {
+        $request1 = $this->getInstance();
+        
+        $this->assertEquals('test', $request1->getUri()->getQuery());
+        $this->assertEquals('', $request1->getHeaderLine('host'));
+        
+        $request2 = $request1->withUri(new Uri('http://example.com/foo'));
+        
+        $this->assertEquals('/foo', $request2->getUri()->getPath());
+        $this->assertEquals('example.com', $request2->getUri()->getHost());
+        $this->assertEquals('example.com', $request2->getHeaderLine('host'));
+        
+        $request3 = $request2->withUri(new Uri('http://example.org/bar'), true);
+        
+        $this->assertEquals('/bar', $request3->getUri()->getPath());
+        $this->assertEquals('example.com', $request3->getHeaderLine('host'));
+        
+        
+    }
+}
