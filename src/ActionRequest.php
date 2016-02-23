a7f76d9a9d2628232c14b7ba99c6fa5f50c94f53
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-02-23 21:26:48 +0100

    Add features to ActionRequest.

--- src/ActionRequest.php
+++ src/ActionRequest.php
@@ -8,7 +8,7 @@ namespace Jivoo\Http;
 /**
  * A server request wrapper for use with {@see Router}.
  */
-class ActionRequest extends Message\Message
+class ActionRequest extends Message\Message implements \Psr\Http\Message\ServerRequestInterface
 {
     
     use Message\RequestTrait, Message\ServerRequestTrait;
@@ -32,6 +32,121 @@ class ActionRequest extends Message\Message
         if (! isset($this->attributes['path'])) {
             $this->attributes['path'] = self::findPath($this);
         }
+        
+        if (! isset($this->attributes['accepts'])) {
+            $this->attributes['accepts'] = [];
+            if (isset($this->server['HTTP_ACCEPT'])) {
+                $contentTypes = explode(',', $this->server['HTTP_ACCEPT']);
+                foreach ($contentTypes as $contentType) {
+                    $contentType = explode(';', $contentType);
+                    $this->attributes['accepts'][] = trim(strtolower($contentType[0]));
+                }
+            }
+        }
+
+        if (! isset($this->attributes['encodings'])) {
+            $this->attributes['encodings'] = [];
+            if (isset($this->server['HTTP_ACCEPT_ENCODING'])) {
+                $acceptEncodings = explode(',', $this->server['HTTP_ACCEPT_ENCODING']);
+                foreach ($acceptEncodings as $encoding) {
+                    $this->attributes['encodings'][] = trim(strtolower($encoding));
+                }
+            }
+        }
+    }
+    
+    /**
+     * Whether or not the client accepts the specified type. If the type is
+     * omitted then a list of acceptable types is returned.
+     *
+     * @param string $type MIME type.
+     * @return bool|string[] True if client accepts provided type, false otherwise.
+     * List of accepted MIME types if type parameter omitted.
+     */
+    public function accepts($type = null)
+    {
+        if (!isset($type)) {
+            return $this->attributes['accepts'];
+        }
+        return in_array($type, $this->attributes['accepts']);
+    }
+
+    /**
+     * Whether or not the client accepts the specified encoding. If the type is
+     * omitted then a list of acceptable encodings is returned.
+     *
+     * @param string $encoding Encoding.
+     * @return bool|string[] True if client accepts provided encoding, false otherwise.
+     * List of accepted encodings if type parameter omitted.
+     */
+    public function acceptsEncoding($encoding = null)
+    {
+        if (!isset($encoding)) {
+            return $this->attributes['encodings'];
+        }
+        return in_array($encoding, $this->attributes['encodings']);
+    }
+
+
+    /**
+     * Whether or not the current request method is GET.
+     *
+     * @return bool True if GET, false if not.
+     */
+    public function isGet()
+    {
+        return $this->method == 'GET';
+    }
+
+    /**
+     * Whether or not the current request method is POST.
+     *
+     * @return bool True if POST, false if not.
+     */
+    public function isPost()
+    {
+        return $this->method == 'POST';
+    }
+
+    /**
+     * Whether or not the current request method is PATCH.
+     *
+     * @return bool True if PATCH, false if not.
+     */
+    public function isPatch()
+    {
+        return $this->method == 'PATCH';
+    }
+
+    /**
+     * Whether or not the current request method is DELETE.
+     *
+     * @return bool True if DELETE, false if not.
+     */
+    public function isDelete()
+    {
+        return $this->method == 'DELETE';
+    }
+
+    /**
+     * Whether or not the current request method is PUT.
+     *
+     * @return bool True if PUT, false if not.
+     */
+    public function isPut()
+    {
+        return $this->method == 'PUT';
+    }
+ 
+    /**
+     * Whether or not the current request was made with AJAX.
+     *
+     * @return bool True if it is, false otherwise.
+     */
+    public function isAjax()
+    {
+        return isset($this->server['HTTP_X_REQUESTED_WITH'])
+            and $this->server['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
     }
     
     public static function findPath(\Psr\Http\Message\ServerRequestInterface $request)
--- tests/ActionRequestTest.php
+++ tests/ActionRequestTest.php
@@ -6,6 +6,35 @@ use Jivoo\TestCase;
 class ActionRequestTest extends TestCase
 {
     
+    public function testAttributes()
+    {
+        $request = Message\Request::create('/index.php/foo/bar')
+            ->withHeader('Foo-Bar', 'foobar')
+            ->withServerParams([
+                'SCRIPT_NAME' => '/index.php',
+                'HTTP_ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'HTTP_ACCEPT_ENCODING' => 'gzip, deflate, sdch',
+                'HTTP_X_REQUESTED_WITH' => 'XMLHttpRequest'
+            ]);
+        $arequest = new ActionRequest($request);
+        
+        $this->assertEquals('foobar', $arequest->getHeaderLine('foo-bar'));
+        $this->assertEquals('foo/bar', $arequest->getAttribute('path'));
+        $this->assertTrue($arequest->accepts('text/html'));
+        $this->assertTrue($arequest->accepts('image/webp'));
+        $this->assertFalse($arequest->accepts('text/plain'));
+        $this->assertCount(5, $arequest->accepts());
+        $this->assertTrue($arequest->acceptsEncoding('gzip'));
+        $this->assertFalse($arequest->acceptsEncoding('bzip2'));
+        $this->assertCount(3, $arequest->acceptsEncoding());
+        $this->assertTrue($arequest->isGet());
+        $this->assertFalse($arequest->isPost());
+        $this->assertFalse($arequest->isPut());
+        $this->assertFalse($arequest->isPatch());
+        $this->assertFalse($arequest->isDelete());
+        $this->assertTrue($arequest->isAjax());
+    }
+    
     public function testFindPath()
     {
         $request = Message\Request::create('/index.php/foo/bar');
