9d25b55f5d8f226ccb1183e27d13db1c156834b9
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-03-08 20:54:37 +0100

    Add tests.

--- tests/Route/NestedMatcherTest.php.php
+++ tests/Route/NestedMatcherTest.php.php
@@ -10,11 +10,13 @@ class NestedMatcherTest extends TestCase
         $route1 = new UrlRoute('foo');
         $route2 = new UrlRoute('bar');
         $route3 = new UrlRoute('baz');
+        $route4 = new UrlRoute('bazbar');
         
         $router = new \Jivoo\Http\Router();
-        $router->match('foo', $route1)
-            ->match('bar', $route2)
-            ->match('baz', $route3);
+        $nested1 = $router->match('foo', $route1);
+        $nested2 = $nested1->match('bar', $route2);
+        $nested2->error($route4);
+        $nested2->match(['baz' => $route3]);
         
         $match = $router->findMatch(['foo'], 'GET');
         $this->assertInstanceOf('Jivoo\Http\Route\UrlRoute', $match);
@@ -27,5 +29,13 @@ class NestedMatcherTest extends TestCase
         $match = $router->findMatch(['foo', 'bar', 'baz'], 'GET');
         $this->assertInstanceOf('Jivoo\Http\Route\UrlRoute', $match);
         $this->assertEquals('baz', $match->getUrl());
+        
+        $match = $router->findMatch(['foo', 'bar', 'ba'], 'GET');
+        $this->assertInstanceOf('Jivoo\Http\Route\UrlRoute', $match);
+        $this->assertEquals('bazbar', $match->getUrl());
+        
+        $match = $router->findMatch(['foo', 'bar', 'baz', 'foo'], 'GET');
+        $this->assertInstanceOf('Jivoo\Http\Route\UrlRoute', $match);
+        $this->assertEquals('bazbar', $match->getUrl());
     }
 }
